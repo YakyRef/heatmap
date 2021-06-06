2c68d661bb8dbf710d1d123af0404fa3bfe95501
Yaky Refael <yaky.ref@gmail.com>
2021-06-06 10:10:45 +0300

    yasoo

--- public/worker.js
+++ public/worker.js
@@ -1,12 +1,3 @@
-const arrayOfWeekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
 onmessage = (e) => {
   const inputData = e.data;
   if (!inputData) {
@@ -21,10 +12,9 @@ onmessage = (e) => {
     while (i < arrayLength) {
       day = new Date(inputData[i]).getDay();
       hour = new Date(inputData[i]).getHours();
-      current = res[arrayOfWeekdays[day]] || null;
+      current = res[day] || null;
       if (!current) {
-        //
-        res[arrayOfWeekdays[day]] = { [hour]: 1 };
+        res[day] = { [hour]: 1 };
       } else {
         if (!current[hour]) {
           current[hour] = 1;
--- src/components/Heatmap/index.js
+++ src/components/Heatmap/index.js
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { arrayOfWeekdays } from "../../constants";
-
 export default function Heatmap({ activity }) {
   const [activityMap, setActivityMap] = useState(null);
+  const [dailyBucketSize, setDailyBucketSize] = useState(24);
 
   useEffect(() => {
-    if (window.Worker) {
-      const myWorker = new Worker("./worker.js");
+    if (window.Worker && activity) {
+      const myWorker = new Worker("worker.js");
       myWorker.postMessage(activity);
       myWorker.onmessage = function (e) {
         setActivityMap(e.data);
         console.log(e.data);
       };
     }
-  }, [activity]);
-  return <div>{activityMap ? "[activityMap[205]]" : "Loading..."}</div>;
+  }, []);
+
+  function renderDayLine(day) {
+    return (
+      <div key={day}>
+        <span>{arrayOfWeekdays[day]} </span>
+        {Array.from({ length: dailyBucketSize }, (x, i) => (
+          <span>{activityMap[day][i] ? activityMap[day][i] : 0} </span>
+        ))}
+      </div>
+    );
+  }
+  return (
+    <div>
+      {activityMap ? Object.keys(activityMap).map(renderDayLine) : "Loading..."}
+    </div>
+  );
 }
