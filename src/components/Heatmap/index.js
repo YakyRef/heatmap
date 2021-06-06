77fef025a9e4797581ca38bbcb01e1bcc927ef40
Yaky Refael <yaky.ref@gmail.com>
2021-06-06 13:14:59 +0300

    boom

--- src/components/Heatmap/index.js
+++ src/components/Heatmap/index.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { arrayOfWeekdays } from "../../constants";
+import "./style.scss";
+
 export default function Heatmap({ activity }) {
   const [activityMap, setActivityMap] = useState(null);
   const [dailyBucketSize, setDailyBucketSize] = useState(24);
+  const [colorsMaxRange, setColorsMaxRange] = useState(250);
 
   useEffect(() => {
     if (window.Worker && activity) {
@@ -15,23 +18,31 @@ export default function Heatmap({ activity }) {
     }
   }, []);
 
-  function getColorByCount(count) {
-    return count;
+  function heatMapColorforValue(heats) {
+    let value = heats > colorsMaxRange ? colorsMaxRange : heats;
+    value = value / colorsMaxRange;
+    var h = (1.0 - value) * 240;
+    return "hsl(" + h + ", 100%, 50%)";
   }
+
+  function getColorByCount(day, hour) {
+    let color = heatMapColorforValue(activityMap[day][hour] || 0);
+
+    return <span className="heatmap__point" style={{ color: color }} />;
+  }
+
   function renderDayLine(day) {
     return (
-      <div key={day}>
-        <span>{arrayOfWeekdays[day]} </span>
-        {Array.from({ length: dailyBucketSize }, (x, i) => (
-          <span>
-            {activityMap[day][i] ? getColorByCount(activityMap[day][i]) : 0}
-          </span>
-        ))}
+      <div key={day} className="heatmap__line">
+        <span className="heatmap__day-title">{arrayOfWeekdays[day]} </span>
+        {Array.from({ length: dailyBucketSize }, (x, i) =>
+          getColorByCount(day, i)
+        )}
       </div>
     );
   }
   return (
-    <div>
+    <div className="heatmap">
       {activityMap ? Object.keys(activityMap).map(renderDayLine) : "Loading..."}
     </div>
   );
